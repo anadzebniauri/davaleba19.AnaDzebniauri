cb87a84783643fcdac6edcab30da944ca2a2a243
anadzebniauri <anadzebniauri13@gmail.com>
2022-07-26 13:12:21 +0400

    Initial Commit

---
+++ davaleba19.AnaDzebniauri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  davaleba19.AnaDzebniauri
+//
+//  Created by Ana Dzebniauri on 26.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
